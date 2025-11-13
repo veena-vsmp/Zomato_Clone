0a655e8bd274e1c76a2f02884c961bba6810a02e
Veena S Malipatil <veenamalipatil279@gmail.com>
2025-11-13 22:06:46 +0530

    menudes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask import request, jsonify, session
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 import os
